007d1e6ba0d1de8c882d7caeca8cf89c896ba6a0
Skyler Over <overtheskyler@gmail.com>
2023-08-08 10:21:40 -0400

    switch to typescript, use existing calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Calendar
-      </header>
-      <table>
-        <thead>Month</thead>
-      </table>
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
     </div>
   );
 }
