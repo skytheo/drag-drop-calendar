21e10b52005ff998bc32a43ba4b9d9fa628aadef
Skyler Over <overtheskyler@gmail.com>
2023-08-08 12:18:47 -0400

    setup calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,80 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { Calendar } from '@fullcalendar/core';
 
 function App() {
+
+const [swimDays, setSwimDays] = useState(-1);
+const [runDays, setRunDays] = useState(-1);
+const [bikeDays, setBikeDays] = useState(-1);
+const [date, setDate] = useState(new Date());
+
+
+useEffect(()=>{
+var calendarEl = document.getElementById('calendar');
+if(calendarEl !== null){
+var calendar = new Calendar(calendarEl as HTMLElement, {
+  plugins: [ dayGridPlugin ],
+  events: [],
+  editable: true
+});
+  calendar.addEvent({title: "Swimming", 
+  date: "2023-08-15",
+  allDay: true})
+}
+},[swimDays]);
+
+const changeSwimDays = (event:any) => {
+  setSwimDays(event.target.value);
+};
+
+const changeBikeDays = (event:any) => {
+  setBikeDays(event.target.value)
+};
+
+const changeRunDays = (event:any) => {
+  setRunDays(event.target.value)
+};
+
+const changeDate = (event:any) => {
+  setDate(event.target.value)
+};
+
   return (
     <div className="App">
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
+      <header>
+        Triathlon Calendar calculator
+      </header>
+      <div>
+        Date of race
+        <input type='date' onChange={changeDate}/>
+      </div>
+        <div>
+          <div>
+            Days a week of swimming:
+            <input type='number' onChange={changeSwimDays}/>
+            //for now swim starts of monday
+          </div>
+          <div>
+            Days a week of running:
+            <input type='number' onChange={changeBikeDays}/>
+            //for now swim starts of wed
+          </div>
+          <div>
+            Days a week of biking:
+            <input type='number' onChange={changeRunDays}/>
+            //for now swim starts of tues
+          </div>
+        </div>
+        {runDays > -1 && bikeDays > -1 && swimDays > -1 &&
+          <div>
+            <FullCalendar
+              plugins={[dayGridPlugin]}
+              initialView="dayGridMonth" />
+          </div>
+        }
     </div>
   );
 }
